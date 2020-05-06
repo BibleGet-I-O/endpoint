4dbe4e8e0eb64e2909ab20d87e1da1d9a6cadb2d
John R. D'Orazio <priest@johnromanodorazio.com>
2020-05-06 12:37:06 +0200

    Update search.php

--- search.php
+++ search.php
@@ -1,2 +1,57 @@
 <?php 
+/**
+ * BibleGet I/O Project Service Endpoint
+ * listens on both GET requests and POST requests
+ * whether ajax or not
+ * accepts all cross-domain requests
+ * is CORS enabled (as far as I understand it)
+ * 
+ * ENDPOINT URL:    https://query.bibleget.io/
+ * 
+ * AUTHOR:          John Romano D'Orazio
+ * AUTHOR EMAIL:    priest@johnromanodorazio.com
+ * AUTHOR WEBSITE:  https://www.johnromanodorazio.com
+ * PROJECT WEBSITE: https://www.bibleget.io
+ * PROJECT EMAIL:   admin@bibleget.io | bibleget.io@gmail.com
+ * 
+ * Copyright John Romano D'Orazio 2014-2021
+ * Licensed under Apache License 2.0
+ * 
+ * This project is meant to contribute to the human community,
+ * the community of mankind itself. 
+ * Considering the Bible is among the oldest writings in the world,
+ * and is the most read book of all human history
+ * containing the wisdom of humanity from the most ancient times
+ * and, for those who have faith, is the inspired Word of God himself
+ * I deemed it necessary and useful to create a project
+ * that would facilitate the usage of the Biblical texts
+ * in the modern digital era.
+ * 
+ * My hope and desire is to be able to add 
+ * as many different versions of the Bible in different languages
+ * as possible, so that all men may have facilitated access to these texts
+ * This project will always only utilize original source texts,
+ * untouched by any third parties, so as to guarantee the authenticity of said texts.
+ *    Deuteronomy 4:2
+ *    "You shall not add to the word which I am commanding you, 
+ *    nor take away from it, that you may keep the commandments 
+ *    of the Lord your God which I command you."
+ * 
+ * I have no desire for any kind of economical advantage
+ * over this project, nobody should speculate eonomically
+ * over the wisdom of humanity or over the Word of God.
+ * May it be of service to mankind. 
+ * While I wish this endpoint engine to be open source,
+ * available to men of good will who might desire to continue this project,
+ * especially Biblical societies around the world,
+ * and I hope the Pontifical Biblical Commission,
+ * I cannot however offer the source texts and the databases they are held in
+ * for public access, they are not all open source, 
+ * they are often covered by copyright by Episcopal Conferences or by Biblical societies.
+ * 
+ * I wish for the code of this engine to be open source,
+ * so that men of good will might contribute to making it better,
+ * more secure, more reliable, to be of better service to mankind.
+ */
+
 ?>
