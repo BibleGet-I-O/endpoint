9e0be714a3ef0da93edc345f96f01cb1b9322ee9
John D'Orazio <priest@johnromanodorazio.com>
2021-10-23 19:03:49 +0200

    substitute all bad query counts with method
    we had created a incrementBadQueryCount method but had not yet substituted all occurrences

--- index.php
+++ index.php
@@ -755,7 +755,7 @@ class BIBLEGET_QUOTE {
                                     /* translators: the expressions <%1$d>, <%2$s>, <%3$s>, and <%4$d> must be left as is, they will be substituted dynamically by values in the script. See http://php.net/sprintf. */
                                     $msg = 'A chapter in the query is out of bounds: there is no chapter <%1$d> in the book %2$s in the requested version %3$s, the last possible chapter is <%4$d>';
                                     $this->addErrorMessage( sprintf( $msg, $match, $thisbook, $jkey, $chapter_limit ) );
-                                    $this->mysqli->query( "UPDATE counter SET bad = bad + 1" );
+                                    $this->incrementBadQueryCount();
                                     continue 3;
                                 }
                             }
@@ -765,14 +765,14 @@ class BIBLEGET_QUOTE {
                         if ( $commacount > 1 ) {
                             if ( !strpos( $query, '-' ) ) {
                                 $this->addErrorMessage( "You cannot have more than one comma and not have a dash!" );
-                                $this->mysqli->query( "UPDATE counter SET bad = bad + 1" );
+                                $this->incrementBadQueryCount();
                                 continue;
                                 //return false;
                             }
                             $parts = explode( "-", $query );
                             if ( count( $parts ) != 2 ) {
                                 $this->addErrorMessage( "You seem to have a malformed querystring, there should be only one dash." );
-                                $this->mysqli->query( "UPDATE counter SET bad = bad + 1" );
+                                $this->incrementBadQueryCount();
                                 continue;
                                 //return false;
                             }
@@ -785,7 +785,7 @@ class BIBLEGET_QUOTE {
                                     if ( $pp[1] > $verselimit ) {
                                         $msg = 'A verse in the query is out of bounds: there is no verse <%1$d> in the book %2$s at chapter <%3$d> in the requested version %4$s, the last possible verse is <%5$d>';
                                         $this->addErrorMessage( sprintf( $msg, $pp[1], $thisbook, $pp[0], $jkey, $verselimit ) );
-                                        $this->mysqli->query( "UPDATE counter SET bad = bad + 1" );
+                                        $this->incrementBadQueryCount();
                                         continue 3;
                                         //return false;
                                     }
@@ -813,7 +813,7 @@ class BIBLEGET_QUOTE {
                                             /* translators: the expressions <%1$d>, <%2$s>, <%3$d>, <%4$s> and %5$d must be left as is, they will be substituted dynamically by values in the script. See http://php.net/sprintf. */
                                             $msg = 'A verse in the query is out of bounds: there is no verse <%1$d> in the book %2$s at chapter <%3$d> in the requested version %4$s, the last possible verse is <%5$d>';
                                             $this->addErrorMessage( sprintf( $msg, $highverse, $thisbook, $parts[0], $jkey, $verselimit ) );
-                                            $this->mysqli->query( "UPDATE counter SET bad = bad + 1" );
+                                            $this->incrementBadQueryCount();
                                             continue 2;
                                             //return false;
                                         }
@@ -832,7 +832,7 @@ class BIBLEGET_QUOTE {
                                             /* translators: the expressions <%1$d>, <%2$s>, <%3$d>, <%4$s> and %5$d must be left as is, they will be substituted dynamically by values in the script. See http://php.net/sprintf. */
                                             $msg = 'A verse in the query is out of bounds: there is no verse <%1$d> in the book %2$s at chapter <%3$d> in the requested version %4$s, the last possible verse is <%5$d>';
                                             $this->addErrorMessage( sprintf( $msg, $highverse, $thisbook, $parts[0], $jkey, $verselimit ) );
-                                            $this->mysqli->query( "UPDATE counter SET bad = bad + 1" );
+                                            $this->incrementBadQueryCount();
                                             continue 2;
                                             //return false;
                                         }
@@ -853,7 +853,7 @@ class BIBLEGET_QUOTE {
                                         /* translators: the expressions <%1$d>, <%2$s>, <%3$d>, <%4$s> and %5$d must be left as is, they will be substituted dynamically by values in the script. See http://php.net/sprintf. */
                                         $msg = 'A verse in the query is out of bounds: there is no verse <%1$d> in the book %2$s at chapter <%3$d> in the requested version %4$s, the last possible verse is <%5$d>';
                                         $this->addErrorMessage( sprintf( $msg, $highverse, $thisbook, $parts[0], $jkey, $verselimit ) );
-                                        $this->mysqli->query( "UPDATE counter SET bad = bad + 1" );
+                                        $this->incrementBadQueryCount();
                                         continue 2;
                                         //return false;
                                     }
@@ -877,7 +877,7 @@ class BIBLEGET_QUOTE {
                         if ( intval( $zchapter ) > $chapter_limit ) {
                             $msg = 'A chapter in the query is out of bounds: there is no chapter <%1$d> in the book %2$s in the requested version %3$s, the last possible chapter is <%4$d>';
                             $this->addErrorMessage( sprintf( $msg, $zchapter, $thisbook, $jkey, $chapter_limit ) );
-                            $this->mysqli->query( "UPDATE counter SET bad = bad + 1" );
+                            $this->incrementBadQueryCount();
                             continue 3;
                         }
                     }
